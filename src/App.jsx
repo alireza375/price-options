419521f5c3744c17c2b6b67ca7aa41ef20569463
alireza375 <alirezaraju8@gmail.com>
2025-03-17 14:53:33 +0600

    graph chart with axios

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import DaisyNav from './components/DaisyNav/DaisyNav'
 import NavBar from './components/NavBar/NavBar'
 import PriceOptions from './components/PriceOptions/PriceOptions'
 import LineChart from './components/LineChart/LineChart'
+import Phones from './components/Phones/Phones'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <NavBar></NavBar>
       <PriceOptions></PriceOptions>
       <LineChart></LineChart>
+      <Phones></Phones> 
     </>
   )
 }
